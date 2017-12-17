a217c7baf8bb6f2902fb4f0add924ed17fcad34a
jasl <jasl9187@hotmail.com>
2017-12-18 00:31:53 +0800

    improve serialization & bump

--- lib/duck_record/serialization.rb
+++ lib/duck_record/serialization.rb
@@ -8,19 +8,18 @@ module DuckRecord #:nodoc:
       self.include_root_in_json = false
     end
 
-    def serializable_hash(options = nil)
-      options = options.try(:dup) || {}
-
-      options[:except] = Array(options[:except]).map(&:to_s)
-
-      super(options)
+    def serializable_hash(options = {})
+      options = (options || {}).reverse_merge except: Array(options[:except]).map(&:to_s)
+      super options
     end
 
     private
 
     def read_attribute_for_serialization(key)
       v = send(key)
-      if v.respond_to?(:to_ary)
+      if v.respond_to?(:serializable_hash)
+        v.serializable_hash
+      elsif v.respond_to?(:to_ary)
         v.to_ary
       elsif v.respond_to?(:to_hash)
         v.to_hash
