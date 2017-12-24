a3eab7d06245dd5e5e119b7462bcfa73d875ea54
jasl <jasl9187@hotmail.com>
2017-12-25 04:59:16 +0800

    minor update

--- lib/duck_record/serialization.rb
+++ lib/duck_record/serialization.rb
@@ -7,14 +7,5 @@ module DuckRecord #:nodoc:
     included do
       self.include_root_in_json = false
     end
-
-    def serializable_hash(options = nil)
-      options = options.try(:dup) || {}
-
-      options[:except] = Array(options[:except]).map(&:to_s)
-      options[:include] = Array(_reflections.keys).map(&:to_s)
-
-      super(options)
-    end
   end
 end
