d3ea126cc2bcf3a3f9b949f752d3d1c04681d7a5
jasl <jasl9187@hotmail.com>
2017-12-25 15:43:19 +0800

    fix serialization

--- lib/duck_record/serialization.rb
+++ lib/duck_record/serialization.rb
@@ -7,5 +7,20 @@ module DuckRecord #:nodoc:
     included do
       self.include_root_in_json = false
     end
+
+    private
+
+    def read_attribute_for_serialization(key)
+      v = send(key)
+      if v.respond_to?(:serializable_hash)
+        v.serializable_hash
+      elsif v.respond_to?(:to_ary)
+        v.to_ary
+      elsif v.respond_to?(:to_hash)
+        v.to_hash
+      else
+        v
+      end
+    end
   end
 end
